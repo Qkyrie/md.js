95862be8b4608cd5961106c39b17108c8eeac148
Quinten De Swaef <quintendeswaef@gmail.com>
2014-09-24 19:30:19 +0200

    ready for use, gonna make documentation now

--- src/md.js
+++ src/md.js
@@ -1,9 +1,23 @@
 var mdjs = {};
 
-mdjs.initializeMarkdown = function () {
+mdjs.init = function (mdjsOptions) {
+
+    mdjsOptions = mdjsOptions || {};
+
+    var mdjsDebug = mdjsOptions.debug;
+    var baseUrl;
+    if(mdjsOptions.baseUrl) {
+        baseUrl = mdjsOptions.baseUrl;
+    } else {
+        baseUrl = "assets/md/";
+    }
+
     $(".mdjs").each(function () {
         var target = $(this);
-        $.get("assets/md/" + $(this).data("md-file"), function (data) {
+        $.get(baseUrl + $(this).data("md-file"), function (data) {
+            if(mdjsDebug) {
+                console.log('got file for target');
+            }
             var converter = new Markdown.Converter();
             target.html(converter.makeHtml(data));
         });
